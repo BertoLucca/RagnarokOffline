2710203f55fa90cc5122927f95d68de4560c73c8
BertoLucca <BertoLucca@users.noreply.github.com>
2022-05-16 00:57:53 -0300

    feat: installer upgrade

--- installer.ps1
+++ installer.ps1
@@ -1,9 +1,33 @@
+$onError = {
+    $server.kill();
+    exit;
+};
+
+function Write-Bar { 
+    param ($after, $before, $separator = '=')
+
+    if ($before.Length -gt 0) {
+        Write-Host $before;
+    }
+    Write-Host $($separator * 80);
+    if ($after.Length -gt 0) {
+        Write-Host $after;
+    }
+};
+
+function Write-ProgressBar {
+    param ($message, $percent)
+    Write-Progress -Activity $message -Status "Progress -> " `
+        -PercentComplete $percent;
+}
+
 # Define path aliases
 $dir = Split-Path ($MyInvocation.MyCommand.Path)
 $sqldir = "$dir/build/mariadb/bin";
 $rathena = "$dir/rathena";
 $build = "$dir/build";
 $3rd = "$dir/3rdparty";
+$translation = "$dir/ROenglishRE";
 
 # Default rAthena connection user
 $username = "ragnarok";
@@ -21,19 +45,63 @@ if (-Not (Test-Path "$build/server")) {
     New-Item -Path "$build/server" -Name "db" -ItemType "directory" | Out-Null;
     New-Item -Path "$build/server" -Name "npc" -ItemType "directory" | Out-Null;
 }
-$items = (Get-Content "$rathena/server-files") | ForEach-Object { "$rathena/$_" };
-Copy-Item -Path $items -Destination "$build/server" -Recurse;
-Copy-Item -Path "$rathena/conf/*" -Destination "$build/server/conf" -Recurse;
-Copy-Item -Path "$rathena/db/*" -Destination "$build/server/db" -Recurse;
-Copy-Item -Path "$rathena/npc/*" -Destination "$build/server/npc" -Recurse;
+
+Write-Bar -before "Starting server files copy...";
+$i = 0;
+Write-ProgressBar "Copying CORE files..." 0;
+($items = Get-Content "$rathena/server-files") | ForEach-Object {
+    $i = $i + 100;
+    Copy-Item -Path "$rathena/$_"  -Destination "$build/server";
+    Write-ProgressBar "Copying CORE files..." $($i/($items.Length));
+};
+Write-Host "CORE files have been copied.";
+
+$i = 0;
+Write-ProgressBar "Copying CONF files..." 0;
+($items = (Get-ChildItem "$rathena/conf" -Recurse -File).FullName ) | 
+ForEach-Object {
+    $i = $i + 100;
+    Copy-Item -Path $_ -Destination "$build/server/conf";
+    Write-ProgressBar "Copying CONF files..."  $($i/($items.Length));
+};
+Write-Host "CONF files have been copied.";
+
+$i = 0;
+Write-ProgressBar "Copying DATABASE files..." 0;
+($items = (Get-ChildItem "$rathena/db" -Recurse -File).FullName ) |
+ForEach-Object {
+    $i = $i + 100;
+    Copy-Item -Path $_ -Destination "$build/server/db";
+    Write-ProgressBar "Copying DATABASE files..." $($i/($items.Length));
+};
+Write-Host "DATABASE files have been copied.";
+
+$i = 0;
+Write-ProgressBar "Copying NPC files..." 0;
+($items = (Get-ChildItem "$rathena/npc" -Recurse -File).FullName ) |
+ForEach-Object {
+    $i = $i + 100;
+    Copy-Item -Path $_ -Destination "$build/server/npc";
+    Write-ProgressBar "Copying NPC files..." $($i/($items.Length));
+};
+Write-Host "NPC files have been copied.";
+Write-Bar "Server files have been copied successfully.";
 
 # unzip the sql client
 Expand-Archive "$3rd/mariadb.zip" -Destination './build';
 # Install db
-& $sqldir/mysql_install_db.exe;
+Write-Bar -before "Starting database instalation...";
+try {
+    & $sqldir/mysql_install_db.exe;
+} catch {
+    Write-Bar "The instalation has failed.";
+    exit;
+};
+Write-Bar "The instalation has been completed";
 
 # start server
-$server = Start-Process -passThru -WindowStyle hidden "$sqldir/mysqld.exe" -ArgumentList "--console";
+$server = Start-Process -passThru -WindowStyle hidden "$sqldir/mysqld.exe" `
+    -ArgumentList "--console";
 
 # wait server isAlive
 Do {
@@ -42,29 +110,77 @@ Do {
 } Until ($code.ExitCode -eq 0);
 
 # Apply sql files
-$instructions = (Get-Content "$dir/scripts/create-user.sql" -Raw) -f ($username, $password);
-$instructions | & $sqldir/mysql.exe -u root;
+try {
+    (Get-Content "$dir/scripts/create-user.sql" -Raw) -f ($username, $password) |
+        & $sqldir/mysql.exe -u root;
+} catch { 
+    &$onError;
+}
 Write-Host "User has been created.";
 
-Write-Host "==================================================================="
 $main_files = Get-ChildItem "$rathena/sql-files/*.sql";
-Write-Host "$($main_files.Length) file(s) will be applied.";
+$total = $main_files.Length + 1
+Write-Host "$total file(s) will be applied.";
 $index = 1;
 foreach ($file in $main_files) {
-    Get-Content $file | & $sqldir/mysql.exe -u $username --password=$password ragnarok;
-    Write-Host "($index of $($main_files.Length))" -NoNewLine -BackgroundColor DarkMagenta;
-    Write-Host " - File ``$($file.Name)`` has been applied.";
+    try {
+        Get-Content $file | & $sqldir/mysql.exe -u $username --password=$password ragnarok;
+    } catch {
+        &$onError;
+    }
+    Write-Host "($index of $total)" -NoNewLine -BackgroundColor DarkMagenta;
+    Write-Host " - File ``$($file.Name)`` has been applied.";   
     $index++;
 }
 
-Get-Content "$dir/scripts/update-login.sql" | & $sqldir/mysql.exe -u $username --password=$password ragnarok;
+try {
+    Get-Content "$dir/scripts/update-login.sql" | 
+        & $sqldir/mysql.exe -u $username --password=$password ragnarok;
+} catch {
+    &$onError;
+};
+Write-Host "($total of $total)" -NoNewLine -BackgroundColor DarkMagenta;
+Write-Host " - File ``update-login.sql`` has been applied.";
 
 # close server connection
 $server.kill();
 
 # Extract client
-Expand-Archive "$3rd/client/kRO_FullClient_20210406.zip" -Destination $build;
+Write-Bar "Starting client unpacking...";
+try {
+    Expand-Archive "$3rd/client/kRO_FullClient_20210406.zip" -Destination $build;
+} catch {
+    &$onError;
+}
 Copy-Item -Path "$build/client/msvcr110.dll" -Destination "$build/server";
+Write-Bar -before "Ragnarok Client has been unpacked.";
+
+# Apply translation
+$i = 0;
+Write-ProgressBar "Copying translation files..." 0;
+($items = (Get-ChildItem "$translation/Renewal" -Recurse -File).FullName ) | 
+ForEach-Object {
+    $i = $i + 100;
+    Copy-Item -Path $_ -Destination "$build/client" -Recurse -Force;
+    Write-ProgressBar "Copying translation files..." $($i/($items.Length));
+}
+Write-Host "Translation has been applied.";
+
+# Remove unnecessary files
+$items = Get-Content "$3rd/client/cleanup" |
+    ForEach-Object { "$build/client/$_" } |
+        Where-Object { Test-Path $_ };
+$i = 0;
+Write-ProgressBar "Cleanup..." 0;
+$items | ForEach-Object {
+    $i = $i + 100;
+    Remove-Item $_ -Recurse -Force;
+    Write-ProgressBar "Cleanup..." $($i/($items.Length));
+}
+
+# Copy pre-patched exe and clientinfo
+Copy-Item "$dir/dev/Ragexe.exe" -Destination "$build/client" -Force;
+Copy-Item "$3rd/client/clientinfo.xml" -Destination "$build/client/data" -Recurse -Force;
 
 # Write utility scripts
 "`"mariadb/bin/mysqld.exe`" --console" | Out-File "$build/start_db.bat" -Encoding ASCII;
