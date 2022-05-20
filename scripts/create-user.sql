51607cc37748f54d22a928273628a65f62962e9b
BertoLucca <BertoLucca@users.noreply.github.com>
2022-05-20 19:04:55 -0300

    feat: user creation and update improvement

--- scripts/create-user.sql
+++ scripts/create-user.sql
@@ -1,9 +1,6 @@
-CREATE USER '{0}'@'localhost' IDENTIFIED BY '{1}';
+CREATE USER 'ragnarok'@'localhost' IDENTIFIED BY 'ragnarok';
 
 CREATE DATABASE ragnarok;
-GRANT ALL ON ragnarok.* TO '{0}'@'localhost' IDENTIFIED BY '{1}';
-
---CREATE DATABASE ragnarok_log;
---GRANT ALL ON ragnarok_log.* TO '{0}'@'localhost' IDENTIFIED BY '{1}';
+GRANT ALL ON ragnarok.* TO 'ragnarok'@'localhost' IDENTIFIED BY 'ragnarok';
 
 FLUSH PRIVILEGES;
--- scripts/update-login.sql
+++ scripts/update-login.sql
@@ -1,3 +1,9 @@
-UPDATE login
-SET userid = 'ragnarok', user_pass = 'password'
+UPDATE `login`
+SET 
+     userid = 'serverconn'
+    ,user_pass = 'password'
+    ,email = ''
 WHERE account_id = 1;
+
+INSERT INTO `login` (`account_id`, `userid`, `user_pass`, `sex`, `group_id`) 
+VALUES (2000000, 'adminacc', 'password', 'M', 99);
