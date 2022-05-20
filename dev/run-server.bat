a32017212117115aae291b542429a6da26bbecae
BertoLucca <BertoLucca@users.noreply.github.com>
2022-05-20 19:04:18 -0300

    feat: add ConEmu

---
+++ dev/run-server.bat
@@ -0,0 +1,5 @@
+start console/ConEmu64.exe -cmdlist ^
+    cmd /k "@echo off & cls & .\mariadb\bin\mysqld.exe --console" -cur_console:fn ^|^|^| ^
+    cmd /k "@echo off & cls & cd server & CALL serv.bat char-server.exe Char-Server %*" -cur_console:s1TVn ^|^|^| ^
+    cmd /k "@echo off & cls & cd server & CALL serv.bat login-server.exe Login-Server %*" -cur_console:s1THn ^|^|^| ^
+    cmd /k "@echo off & cls & cd server & CALL serv.bat map-server.exe Map-Server %*" -cur_console:s2THn
