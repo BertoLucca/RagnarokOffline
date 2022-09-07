218636dd66a66e4e1b4966163cdd37bbb42761a0
BertoLucca <BertoLucca@users.noreply.github.com>
2022-09-06 21:38:40 -0300

    fix: script calls and OpenSetup support

--- scripts/hta/run-db.bat
+++ scripts/hta/run-db.bat
@@ -1,3 +1,4 @@
+@echo off
 cd %~dp0
 ..\mariadb\bin\mysqld.exe --console
 exit
--- scripts/hta/run-repair.bat
+++ scripts/hta/run-repair.bat
@@ -1,3 +1,4 @@
+@echo off
 cd %~dp0
-..\mariadb\bin\mysqlcheck.exe --repair --all-databases
+..\mariadb\bin\mysqlcheck.exe -u root --auto-repair --check --all-databases
 pause
